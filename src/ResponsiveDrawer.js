98bde7bc9be4bedaea65bd785834468c896ce83d
Léna Guitou <100426022+lenaguitou@users.noreply.github.com>
2022-04-04 17:38:31 +0200

    Update ResponsiveDrawer.js

--- src/ResponsiveDrawer.js
+++ src/ResponsiveDrawer.js
@@ -46,7 +46,7 @@ import function_editor_options from './assets/images/function_editor_options.png
 import dispersion from './assets/images/dispersion.png';
 import phases_add from './assets/images/phases_add.png';
 import phases_graph from './assets/images/phases_graph.png';
-import phases_params from './assets/images/phases_params.png';
+import phases_parameters from './assets/images/phases_parameters.png';
 import division_shift from './assets/images/division_shift.PNG';
 import division_dispersion from './assets/images/division_dispersion.png';
 import initialization_file from './assets/images/initialization_file.png';
@@ -1122,7 +1122,7 @@ function ManualContent(props) {
             The maximum value of the ordinate axis in the graph can be changed by editing the lateral textbox.
           </Typography>
           <Typography paragraph align="center">
-            <img alt="" src={phases_params} />
+            <img alt="" src={phases_parameters} />
           </Typography>
 
           <Typography paragraph>
